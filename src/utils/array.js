aeee3750fbcf2b9a588fbef3b36ba95e19be9921
kristjanjansen <kristjan.jansen@gmail.com>
2020-03-15 19:55:57 +0100

    props

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,3 +1,6 @@
+export const flatten = list =>
+  list.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
+
 export const padArrayRight = (arr, length, fill) => {
   return [...arr, ...Array(length).fill(fill)].slice(0, length);
 };
