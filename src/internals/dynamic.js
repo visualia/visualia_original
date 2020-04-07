1b250884c3557967eb2ef41f35d918cf5666f3de
kristjanjansen <kristjan.jansen@gmail.com>
2020-04-07 21:52:58 +0200

    autofill

--- src/internals/dynamic.js
+++ src/internals/dynamic.js
@@ -1,18 +1,25 @@
 export const dynamicProps = {
+  set: {
+    default: "",
+    autofill: "a",
+    type: String,
+    docs: "Global variable name to set",
+  },
   from: {
     default: 0,
+    autofill: "0",
     type: [String, Number],
-    docs: "Starting value"
+    docs: "Starting value",
   },
   to: {
     default: 360,
+    autofill: "360",
     type: [String, Number],
-    docs: "Ending value"
+    docs: "Ending value",
   },
   integer: {
     default: false,
     type: [Boolean, String],
-    docs: "Output an interger value (instead floating point)?"
+    docs: "Output an interger value (instead floating point)?",
   },
-  set: { default: "", type: String, docs: "Global variable name to set" }
 };
