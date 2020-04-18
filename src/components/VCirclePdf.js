4b38376b68fa7bc8466f85e3cd94c5450122386f
kristjanjansen <kristjan.jansen@gmail.com>
2020-04-19 00:28:12 +0200

    clear

--- src/components/VCirclePdf.js
+++ src/components/VCirclePdf.js
@@ -1,18 +1,14 @@
 import { inject } from "../deps/vue.js";
+import { toNumber } from "../utils.js";
 import { stylingPdf, parseCoords } from "../internals.js";
 
 export const VCirclePdf = {
   setup(props) {
     const sceneContext = inject("sceneContext");
-    const position = parseCoords(props.position)[0];
+    const [x, y] = parseCoords(props.position)[0];
     if (sceneContext.pdf.value) {
       const { pdfStyle } = stylingPdf(props, sceneContext.pdf.value);
-      sceneContext.pdf.value.circle(
-        position[0],
-        position[1],
-        props.r,
-        pdfStyle
-      );
+      sceneContext.pdf.value.circle(x, y, toNumber(props.r), pdfStyle);
       sceneContext.update();
     }
     return () => null;
--- src/experimental/index.md
+++ src/experimental/index.md
@@ -3,15 +3,15 @@
 r is is {{ get("r") }}
 
 <v-scene mode="pdf">
-  <v-line points="0 0, 50 50, 0 25" :fill="[255,255,0]" />
   <v-square
-    :r="get('r')"
+    r="5"
     :fill="[0,0,255]"
     position="50 50"
   />
+  <v-line points="0 0, 50 50, 0 25" :fill="[255,255,0]" />
   <v-circle
-    :r="get('r')"
+    r="10"
     :fill="[255,0,0]"
-    position="100 100"
+    position="150 100"
   />
 </v-scene>
