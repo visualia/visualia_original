dbb1d4b5367af0c861b86dad75068abc89439444
kristjanjansen <kristjan.jansen@gmail.com>
2020-04-18 20:22:11 +0200

    clear

--- src/components/VSquarePdf.js
+++ src/components/VSquarePdf.js
@@ -1,21 +1,22 @@
-import { inject, watch } from "../deps/vue.js";
-import { stylingPdf } from "../internals.js";
+import { inject } from "../deps/vue.js";
+import { parseCoords, stylingPdf } from "../internals.js";
 
 export const VSquarePdf = {
   setup(props) {
     const sceneContext = inject("sceneContext");
-    watch(() => {
-      if (sceneContext.pdf.value) {
-        stylingPdf(props, sceneContext.pdf.value);
-        sceneContext.pdf.value.rect(
-          -props.r,
-          -props.r,
-          props.r * 2,
-          props.r * 2
-        );
-        sceneContext.update();
-      }
-    });
+    const position = parseCoords(props.position)[0];
+    if (sceneContext.pdf.value) {
+      const { pdfStyle } = stylingPdf(props, sceneContext.pdf.value);
+      stylingPdf(props, sceneContext.pdf.value);
+      sceneContext.pdf.value.rect(
+        position[0] - props.r,
+        position[1] - props.r,
+        position[0] + props.r * 2,
+        position[1] + props.r * 2,
+        pdfStyle
+      );
+      sceneContext.update();
+    }
     return () => null;
   },
 };
--- src/experimental/index.md
+++ src/experimental/index.md
@@ -3,13 +3,15 @@
 r is is {{ get("r") }}
 
 <v-scene mode="pdf">
+  <v-line points="0 0, 50 50, 0 25" :fill="[255,255,0]" />
+  <v-square
+    :r="get('r')"
+    :fill="[0,0,255]"
+    position="50 50"
+  />
   <v-circle
     :r="get('r')"
     :fill="[255,0,0]"
     position="100 100"
   />
-  <v-square
-    :r="get('r')"
-  />
-  <v-line points="0 0, 50 50, 0 25" :fill="[255,255,0]" />
 </v-scene>
