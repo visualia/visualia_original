a3e87257182dc36f54cdd7149908e874365815b8
kristjanjansen <kristjan.jansen@gmail.com>
2020-04-25 06:32:06 +0200

    Polygon fill

--- src/components/VLineThree.js
+++ src/components/VLineThree.js
@@ -6,9 +6,21 @@ import {
   LineBasicMaterial,
   BufferGeometry,
   Line,
+  Shape,
+  ShapeGeometry,
+  Vector2,
+  Mesh,
 } from "../deps/three.js";
 
-import { parseCoords, useThreeTransform } from "../internals.js";
+import { parseCoords, useThreeTransform, useThreeFill } from "../internals.js";
+
+// const polygonGeometry = (points) => {
+//   const geometry = new Geometry();
+//   points.forEach((p) => geometry.vertices.push(new Vector3(...p)));
+//   geometry.faces.push(new Face3(0, 1, 2));
+//   geometry.computeFaceNormals();
+//   return geometry;
+// };
 
 export const VLineThree = {
   setup(props) {
@@ -18,21 +30,32 @@ export const VLineThree = {
     if (props.closed) {
       parsedPoints = [...parsedPoints, parsedPoints[0]];
     }
-    const geometry = new BufferGeometry().setFromPoints(
-      parsedPoints.map((point) => new Vector3(...point))
-    );
 
     var group = new Group();
 
-    const stroke = new LineBasicMaterial({
-      color: props.stroke,
-      linewidth: props.strokeWidth,
-      opacity: props.opacity,
-    });
+    if (props.fill !== "none") {
+      const shape = new Shape(parsedPoints.map((p) => new Vector2(p[0], p[1])));
+      const geometry = new ShapeGeometry(shape);
+      const fill = useThreeFill(props);
+      const fillObject = new Mesh(geometry, fill.value);
+      group.add(fillObject);
+    }
 
-    const line = new Line(geometry, stroke);
+    if (props.stroke !== "none") {
+      const geometry = new BufferGeometry().setFromPoints(
+        parsedPoints.map((point) => new Vector3(...point))
+      );
 
-    group.add(line);
+      const stroke = new LineBasicMaterial({
+        color: props.stroke,
+        linewidth: props.strokeWidth,
+        opacity: props.opacity,
+      });
+
+      const line = new Line(geometry, stroke);
+
+      group.add(line);
+    }
 
     sceneContext.scene.add(group);
 
