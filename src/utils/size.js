eb4d2e18b76fa43dd899856576cd3237ecca470d
kristjanjansen <kristjan.jansen@gmail.com>
2020-05-31 19:42:09 +0200

    cleanup

---
+++ src/utils/size.js
@@ -0,0 +1,15 @@
+import { ref, onMounted } from "../deps/vue.js";
+
+export const useSize = () => {
+  const el = ref(null);
+  const width = ref(null);
+  const height = ref(null);
+  onMounted(() => {
+    const observer = new ResizeObserver(async (entries) => {
+      width.value = entries[0].contentRect.width;
+      height.value = entries[0].contentRect.height;
+    });
+    observer.observe(el.value);
+  });
+  return { el, width, height };
+};
