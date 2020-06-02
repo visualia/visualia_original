f0000952102ad733bad606c0c7c97f75e88058b1
kristjanjansen <kristjan.jansen@gmail.com>
2020-06-02 18:43:08 +0200

    better case

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ const files = [
 const content = `
 <v-scene v-for="m in ['svg','canvas','three','webgl','pdf']" :mode="m">
   <v-line points="10 10, 30 40" position="100 100" width="50" height="50" />
+  <v-polygon points="10 10, 30 40" position="50 50" width="50" height="50" />
   <v-rect r="50" position="100 100" width="50" height="50" />
+  <v-hexagon r="50" position="100 100" width="50" height="50" />
   <v-square r="50" position="100 100" width="50" height="50" />
   <v-circle r="50" position="100 100" width="50" height="50" />
 </v-scene>
