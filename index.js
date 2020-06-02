72ac5a2a0e453565f590a7c46ef19bf83dc7ebd3
kristjanjansen <kristjan.jansen@gmail.com>
2020-06-02 19:20:48 +0200

    better testcase

--- index.js
+++ index.js
@@ -25,12 +25,18 @@ const files = [
 
 const content = `
 <v-scene v-for="m in ['svg','canvas','three','webgl','pdf']" :mode="m">
+  <v-group position="100 15">
+    <v-point />
+    <v-circle />
+  </v-group>
+  <v-point position="20 25" />
   <v-line points="10 10, 30 40" position="100 100" width="50" height="50" />
   <v-polygon points="10 10, 30 40" position="50 50" width="50" height="50" />
   <v-rect r="50" position="100 100" width="50" height="50" />
   <v-hexagon r="50" position="100 100" width="50" height="50" />
   <v-square r="50" position="100 100" width="50" height="50" />
   <v-circle r="50" position="100 100" width="50" height="50" />
+  <v-sphere r="10" position="150 150" width="50" height="50" />
 </v-scene>
 `;
 visualia({
