ff01d122ea9be6c074249c28466d461ab6d001ad
kristjanjansen <kristjan.jansen@gmail.com>
2020-06-04 20:23:01 +0200

    Adding test case

--- index.js
+++ index.js
@@ -23,45 +23,7 @@ const files = [
   "./docs/faq.md",
 ];
 
-const content = `
-
-### Hello
-
-<v-slider set="a" to="100" />{{ get('a') }}
-<v-scene v-for="m in ['svg','canvas','three','webgl','pdf']" :mode="m">
-  <v-group position="100 15">
-    <v-point />
-    <v-circle />
-  </v-group>
-  <v-circle r="20" :position="[get('a',0), 50]" />
-  <v-line points="10 10, 30 40" position="100 100" width="50" height="50" />
-  <v-polygon points="10 10, 30 40" position="50 50" width="50" height="50" />
-  <v-rect r="50" position="100 100" width="50" height="50" />
-  <v-hexagon r="50" position="100 100" width="50" height="50" />
-  <v-square r="50" position="100 100" width="50" height="50" />
-  <v-circle r="50" position="100 100" width="50" height="50" />
-  <v-sphere r="10" position="150 150" width="50" height="50" />
-</v-scene>
-
-### world
-
-`;
-
-const content2 = `
-<v-slider set="b" />
-
-b is {{ get("b") }}
-
-<v-scene v-for="m in ['svg','canvas','three']" :mode="m">
-  <v-square
-    position="100 100"
-    r="50"
-    :rotation="get('b')"
-  />
-  <v-circle r="100" />
-</v-scene>
-`;
 visualia({
-  content: content,
+  files,
   components: { PfiveExample, ObservableExample },
 });
