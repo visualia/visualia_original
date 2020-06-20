1b191063aad062904ac77b7d802d624186598cc3
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-06-21 01:07:52 +0200

    feat: moving all built code to dist (#67)

--- rollup.deps.config.js
+++ rollup.deps.config.js
@@ -3,7 +3,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-porter";
-//import url from "@rollup/plugin-url";
 import copy from "rollup-plugin-copy";
 
 const deps = [
@@ -22,17 +21,17 @@ export default [
   ...deps.map((dep) => ({
     input: `./deps/${dep}.js`,
     output: {
-      file: `./src/deps/${dep}.js`,
+      file: `./dist/deps/${dep}.js`,
       format: "es",
     },
     plugins: [
       resolve(),
       commonjs(),
       json(),
-      css({ dest: "./dist/katex.css", raw: false }),
+      css({ dest: "./dist/deps/katex.css", raw: false }),
       copy({
         targets: [
-          { src: "node_modules/katex/dist/fonts/*", dest: "dist/fonts" },
+          { src: "node_modules/katex/dist/fonts/*", dest: "dist/deps/fonts" },
         ],
       }),
       terser(),
@@ -41,18 +40,18 @@ export default [
   {
     input: "./deps/monaco.js",
     output: {
-      dir: "./src/deps/monaco",
+      dir: "./dist/deps/monaco",
       format: "es",
       chunkFileNames: "[name].js",
     },
     plugins: [
-      css({ dest: "./dist/monaco.css", raw: false }),
+      css({ dest: "./dist/deps/monaco/monaco.css", raw: false }),
       copy({
         targets: [
           {
             src:
               "node_modules/monaco-editor/esm/vs/base/browser/ui/codiconLabel/codicon/codicon.ttf",
-            dest: "dist",
+            dest: "dist/deps/monaco",
           },
         ],
       }),
@@ -64,7 +63,7 @@ export default [
   {
     input: "node_modules/monaco-editor/esm/vs/editor/editor.worker.js",
     output: {
-      file: "./src/deps/monaco/editor.worker.js",
+      file: "./dist/deps/monaco/editor.worker.js",
       format: "umd",
       name: "editor",
     },
