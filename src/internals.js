2c7aa67496a676aea2c4405ae011ab0dd5736b24
kristjanjansen <kristjan.jansen@gmail.com>
2020-07-31 15:21:37 +0300

    feat: suspense with error catching

--- src/internals.js
+++ src/internals.js
@@ -13,6 +13,7 @@ export { default as VMenu } from "./internals/VMenu.js";
 export { default as VMenuIcon } from "./internals/VMenuIcon.js";
 export { default as VCompiler } from "./internals/VCompiler.js";
 
+export { default as VSuspense } from "./internals/VSuspense.js";
 export { default as VSection } from "./internals/VSection.js";
 export { default as VContent } from "./internals/VContent.js";
 export { default as VDocumentIcon } from "./internals/VDocumentIcon.js";
