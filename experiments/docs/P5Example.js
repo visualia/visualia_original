ab70e6a8098a1d7b357e8f7bde5c3c5074298b8c
kristjanjansen <kristjan.jansen@gmail.com>
2020-07-31 12:59:28 +0300

    chore: cleanup

--- experiments/docs/P5Example.js
+++ experiments/docs/P5Example.js
@@ -2,13 +2,6 @@ import { get } from "../../dist/visualia.js";
 import { ref, onMounted } from "../../dist/deps/vue.js";
 import p5 from "https://cdn.skypack.dev/pin/p5@v1.1.9-QZRc7UPZWx2v3Qaby8jl/min/p5.js";
 
-// p5 sketch
-
-// Note that you need to wrap into the sketch function
-// and prefix all commands with s, otherwise it is regular p5 API
-
-// Note that we use get() function to use Visualia live variables
-
 const sketch = (s) => {
   s.setup = () => {
     s.createCanvas(200, 200);
@@ -21,10 +14,8 @@ const sketch = (s) => {
   };
 };
 
-// We are creating a wrapper component <pfive-example />
-
 export default {
-  async setup() {
+  setup() {
     const el = ref(null);
     onMounted(() => {
       new p5(sketch, el.value);
