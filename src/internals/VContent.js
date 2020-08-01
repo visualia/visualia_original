d9b20cf9e098cb5466ae992e652db6ab3fca6a06
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-08-01 08:45:12 +0300

    feat: layout component (#86)

--- src/internals/VContent.js
+++ src/internals/VContent.js
@@ -14,7 +14,7 @@ import { slug, useSize } from "../utils.js";
 import {
   VMenu,
   VMenuIcon,
-  VCompiler,
+  VLayout,
   VSection,
   parseContent,
   sectionGridStyle,
@@ -22,7 +22,7 @@ import {
 } from "../internals.js";
 
 export default {
-  components: { VSection, VMenu, VMenuIcon },
+  components: { VLayout, VSection, VMenu },
   props: {
     content: {
       default: "",
@@ -37,23 +37,6 @@ export default {
   },
   setup(props) {
     const router = inject("router");
-
-    const { el, width } = useSize();
-    const isMobile = computed(() => width.value < 800);
-
-    const showMenu = ref(false);
-    const unwatch = watch(
-      () => width.value,
-      () => {
-        if (width.value !== null) {
-          showMenu.value = !isMobile.value;
-          if (unwatch) {
-            unwatch();
-          }
-        }
-      },
-      { immediate: true }
-    );
 
     const setSectionTitle = (section, i) => {
       if (!section.title) {
@@ -108,68 +91,19 @@ export default {
       parsedContent,
       contentMenu,
       sectionGridStyle,
-      el,
-      showMenu,
-      isMobile,
     };
   },
   template: `
-  <div
-    ref="el"
-    style="display: flex; position: relative;"
-    :style="{'--base': isMobile ? '7px' : '8px'}"
-  >
-    <div v-if="menu && !isMobile && showMenu" style="width: 250px; background: gray;"></div>
-    <div v-if="menu && isMobile && showMenu"
-      style="
-        z-index: 1000;
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        right: 0;
-        width: calc(100vw - 250px);
-        background: rgba(0,0,0,0.2);
-      "
-      @click="showMenu = !showMenu"
-    />
-    <div v-if="menu && showMenu"
-      :style="{
-        boxShadow: isMobile ? '0 0 20px hsla(200, 19%, 28%, 0.5)' : ''
-      }"
-      style="
-      z-index: 1000;
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      width: 250px;
-      overflow: auto;
-      background: white;
-    ">
+  <v-layout :menu="menu">
+    <template #menu>
       <v-menu :menu="contentMenu" />
-    </div>
-    <div
-      v-if="menu"
-      style="
-        position: fixed;
-        top: 0px;
-        left: 10px;
-        z-index: 10000;
-        font-size: 32px;
-        cursor: pointer;
-        opacity: 0.75;
-      "
-      @click="showMenu = !showMenu"
-    >
-      <v-menu-icon />
-    </div>
-    <div style="flex: 1; position: relative; display: flex; justify-content: center;">
-      <div  style="max-width: 900px; width: 100%; padding-top: var(--base2)">
-        <template v-for="(section,i) in visibleContent">
-          <v-section :key="i" :section="section" />
-        </template>
-      </div>
-    </div>
-  </div>
+    </template>
+    <template #content>
+      <template v-for="(section,i) in visibleContent">
+        {{ section.visible }}
+        <v-section :key="i" :section="section" />
+      </template>
+    </template>
+  </v-layout>
   `,
 };
